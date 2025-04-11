aa186f462c6dad6ccc501ec1c14cfeab64851849
nandini <nandu.v.kumar05@gmail.com>
2025-04-11 11:51:25 +0530

    new

--- python.py
+++ python.py
@@ -1,9 +1,9 @@
 # Store input numbers
-num1 = int(input('Enter first number: '))
-num2 = int(input('Enter second number: '))
+num3 = int(input('Enter first number: '))
+num4= int(input('Enter second number: '))
 
 # Add two numbers
-sum = float(num1 + num2)
+sum = float(num3 + num4)
 
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
